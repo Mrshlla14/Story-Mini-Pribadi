696174c0da833009c62f8908974877656347a13c
Mrshlla14 <148568715+Mrshlla14@users.noreply.github.com>
2025-05-15 02:13:50 +0700

    Add files via upload

---
+++ MyStoryHub/src/scripts/routes/routes.js
@@ -0,0 +1,19 @@
+import RegisterPage from '../pages/profile/register/register-page';
+import LoginPage from '../pages/profile/login/login-page';
+import HomePage from '../pages/home/home-page';
+import BookmarkPage from '../pages/bookmark/bookmark-page';
+//import StoryDetailPage from '../pages/story-detail/story-detail-page';
+import NewPage from '../pages/add/add-page';
+import WelcomePage from '../pages/beranda/beranda-page';
+import { checkAuthenticatedRoute, checkUnauthenticatedRouteOnly } from '../utils/profile';
+
+export const routes = {
+  '/': () => new WelcomePage(),
+  '/login': () => checkUnauthenticatedRouteOnly(new LoginPage()),
+  '/register': () => checkUnauthenticatedRouteOnly(new RegisterPage()),
+
+  '/home': () => checkAuthenticatedRoute(new HomePage()),
+  '/new': () => checkAuthenticatedRoute(new NewPage()),
+ // '/stories/:id': () => checkAuthenticatedRoute(new StoryDetailPage()),
+  '/bookmark': () => checkAuthenticatedRoute(new BookmarkPage()),
+};
