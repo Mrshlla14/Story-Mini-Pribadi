ea1392b57bf475a042e309075fc028fefa8c7a04
Mrshlla14 <148568715+Mrshlla14@users.noreply.github.com>
2025-05-15 02:11:38 +0700

    Delete Story-mini-pribadi directory

--- Story-mini-pribadi/src/scripts/config.js
+++
@@ -1,9 +0,0 @@
-
-// Authentication
-export const ACCESS_TOKEN_KEY = 'accessToken';
-
-// API endpoints
-export const BASE_URL = 'https://story-api.dicoding.dev/v1';
-
-// Map service
-export const MAP_SERVICE_API_KEY = 'YOUR_KEY';
